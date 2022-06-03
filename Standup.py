79b99205fadf59f2740767c91221106e3095dc10
Scott D. McVay <nodtonothing@gmail.com>
2022-06-03 13:28:46 -0500

    Update Standup.py
    This adds pickling so you don't get the same person two times in a row (as long as you don't wipe your pickle),.

--- Standup.py
+++ Standup.py
@@ -1,12 +1,29 @@
+import os.path
+import pickle
 import random
+from os import path
+
 import pyfiglet
 
-names = ['Joe', 'Jessica', 'David', 'Alora', 'Martine', 'JoeBob', 'Amelie', 'Sauron', 'Squirtl', 'Naruto', 'Scott']
+names = ['Alora', 'Grant', 'Sam', 'Arnela','Monika', 'Manager\'s Choice', 'Chris', 'Nudzejma', 'Mirza', 'Jeeka']
 selected_name = random.choice(names)
 
+#might err the first time
+selected_name_last = ""
+if os.path.exists("save.p"):
+    if os.path.getsize("save.p") > 0:
+        selected_name_last = pickle.load( open( "save.p", "rb" ) )
+
+while selected_name == selected_name_last:
+    selected_name = random.choice(names)
+
+pickle.dump( selected_name, open( "save.p", "wb" ) )
+
+#should write it and not use it next time (pickle)
 color_list = list(pyfiglet.COLOR_CODES.keys())
 
 color_list.remove('BLACK')
+color_choice = pyfiglet.COLOR_CODES.keys
 color_choice = random.choice(color_list)
 font_list = pyfiglet.FigletFont.getFonts()
 font_choice = random.choice(font_list)
